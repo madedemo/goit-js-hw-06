fcbb193d8011745ce6c5ede2444609aada6ca2b9
madedemo <madelka518@gmail.com>
2023-12-16 10:29:19 +0300

    Update task-3.js

--- js/task-3.js
+++ js/task-3.js
@@ -1,22 +1,23 @@
 class StringBuilder {
+  #value;
   constructor(initialValue) {
-    this.value = initialValue;
+    this.#value = initialValue;
   }
 
   getValue() {
-    return this.value;
+    return this.#value;
   }
 
   padStart(str) {
-    this.value = str + this.value;
+    this.#value = `${str}${this.#value}`;
   }
 
   padEnd(str) {
-    this.value = this.value + str;
+    this.#value = `${this.#value}${str}`;
   }
 
   padBoth(str) {
-    this.value = str + this.value + str;
+    this.#value = `${str}${this.#value}${str}`;
   }
 }
 
